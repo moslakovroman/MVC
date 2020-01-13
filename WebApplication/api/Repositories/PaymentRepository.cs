23e5a15e1031b21e6f63f22528ff773b27faccb4
Roman <litl_rom@tut.by>
2020-01-13 10:21:46 +0300

    new viewmodel2

--- WebApplication/api/Repositories/PaymentRepository.cs
+++ WebApplication/api/Repositories/PaymentRepository.cs
@@ -10,96 +10,228 @@ namespace api.Repositories
     public class PaymentRepository: IRepoPayment
 
     {
-        public List<Payment> payments;
-        
+        //public List<Payment> payments;
+
         public List<Payment> GetPurchase()
         {
-            payments = new List<Payment>();
+            //payments = new List<Payment>();
 
-            payments.Add(new Payment()
-            {
-                ProductId = 1,
-                Id = 3,
-                UserId = 3,
-                Quantity = 3
-            });
+            //payments.Add(new Payment()
+            //{
+            //    ProductId = 1,
+            //    Id = 3,
+            //    UserId = 3,
+            //    Quantity = 3
+            //});
 
-            payments.Add(new Payment()
-            {
-                ProductId = 2,
-                Id = 1,
-                UserId = 2,
-                Quantity = 5
-            });
+            //payments.Add(new Payment()
+            //{
+            //    ProductId = 2,
+            //    Id = 1,
+            //    UserId = 2,
+            //    Quantity = 5
+            //});
 
-            payments.Add(new Payment()
-            {
-                ProductId = 3,
-                Id = 2,
-                UserId = 1,
-                Quantity = 8
-            });
-            return payments;
+            //payments.Add(new Payment()
+            //{
+            //    ProductId = 3,
+            //    Id = 2,
+            //    UserId = 1,
+            //    Quantity = 8
+            //});
+            return null;/*payments*/;
         }
 
-        private List<User> users;
-        
-        private List<Product> products;
-        
-        public PaymentRepository()
+        private static List<Payment> _payments = InitPayment();
+
+        private static List<Payment> InitPayment()
         {
-            products = new List<Product>();
+            return new List<Payment>()
+            {
+                new Payment() {ProductId = 1, Id = 3, UserId = 3, Quantity = 3},
+                new Payment() {ProductId = 2, Id = 1, UserId = 2, Quantity = 5},
+                new Payment() {ProductId = 3, Id = 2, UserId = 1, Quantity = 8}
+            };
+            
 
-            products.Add(new Product()
-            {
-                Id = 1,
-                Name = "Phone",
-                Price = 300,
-                Description = "Samsung"
-            });
-            products.Add(new Product()
-            {
-                Id = 2,
-                Name = "Notebook",
-                Price = 2300,
-                Description = "MSI"
-            });
-            products.Add(new Product()
-            {
-                Id = 3,
-                Name = "Monitor",
-                Price = 500,
-                Description = "Dell"
-            });
+            //payments.Add(new Payment()
+            //{
+            //    ProductId = 1,
+            //    Id = 3,
+            //    UserId = 3,
+            //    Quantity = 3
+            //});
 
-            users = new List<User>();
+            //payments.Add(new Payment()
+            //{
+            //    ProductId = 2,
+            //    Id = 1,
+            //    UserId = 2,
+            //    Quantity = 5
+            //});
 
-            users.Add(new User()
-            {
-                Login = "lopata",
-                Age = 30,
-                Id = 1
-            });
-            users.Add(new User()
-            {
-                Login = "chehol",
-                Age = 20,
-                Id = 2
-            });
-            users.Add(new User()
-            {
-                Login = "holop",
-                Age = 25,
-                Id = 3
-            });
+            //payments.Add(new Payment()
+            //{
+            //    ProductId = 3,
+            //    Id = 2,
+            //    UserId = 1,
+            //    Quantity = 8
+            //});
         }
+
+        private static List<User> _users = InitUsers();
+
+        private static List<User> InitUsers()
+        {
+            return _users = new List<User>()
+            {
+                new User() {Login = "lopata", Age = 30, Id = 1},
+                new User() {Login = "chehol", Age = 20, Id = 2},
+                new User() {Login = "holop", Age = 25, Id = 3}
+            };
+            
+
+            //users.Add(new User()
+            //{
+            //    Login = "lopata",
+            //    Age = 30,
+            //    Id = 1
+            //});
+            //users.Add(new User()
+            //{
+            //    Login = "chehol",
+            //    Age = 20,
+            //    Id = 2
+            //});
+            //users.Add(new User()
+            //{
+            //    Login = "holop",
+            //    Age = 25,
+            //    Id = 3
+            //});
+           
+        }
+
+        private static List<Product> _products = InitProducts();
+
+        private static List<Product> InitProducts()
+        {
+            return _products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Phone", Price = 300, Description = "Samsung" },
+                new Product() { Id = 2, Name = "Notebook", Price = 2300, Description = "MSI" },
+                new Product() { Id = 3, Name = "Monitor", Price = 500, Description = "Dell" }
+            };
+            
+
+            //products.Add(new Product()
+            //{
+            //    Id = 1,
+            //    Name = "Phone",
+            //    Price = 300,
+            //    Description = "Samsung"
+            //});
+            //products.Add(new Product()
+            //{
+            //    Id = 2,
+            //    Name = "Notebook",
+            //    Price = 2300,
+            //    Description = "MSI"
+            //});
+            //products.Add(new Product()
+            //{
+            //    Id = 3,
+            //    Name = "Monitor",
+            //    Price = 500,
+            //    Description = "Dell"
+            //});
+            
+        }
+
+        
+
+        //public PaymentRepository()
+        //{
+        //    products = new List<Product>();
+
+        //    products.Add(new Product()
+        //    {
+        //        Id = 1,
+        //        Name = "Phone",
+        //        Price = 300,
+        //        Description = "Samsung"
+        //    });
+        //    products.Add(new Product()
+        //    {
+        //        Id = 2,
+        //        Name = "Notebook",
+        //        Price = 2300,
+        //        Description = "MSI"
+        //    });
+        //    products.Add(new Product()
+        //    {
+        //        Id = 3,
+        //        Name = "Monitor",
+        //        Price = 500,
+        //        Description = "Dell"
+        //    });
+
+        //    users = new List<User>();
+
+        //    users.Add(new User()
+        //    {
+        //        Login = "lopata",
+        //        Age = 30,
+        //        Id = 1
+        //    });
+        //    users.Add(new User()
+        //    {
+        //        Login = "chehol",
+        //        Age = 20,
+        //        Id = 2
+        //    });
+        //    users.Add(new User()
+        //    {
+        //        Login = "holop",
+        //        Age = 25,
+        //        Id = 3
+        //    });
+        //    payments = new List<Payment>();
+
+        //    payments.Add(new Payment()
+        //    {
+        //        ProductId = 1,
+        //        Id = 3,
+        //        UserId = 3,
+        //        Quantity = 3
+        //    });
+
+        //    payments.Add(new Payment()
+        //    {
+        //        ProductId = 2,
+        //        Id = 1,
+        //        UserId = 2,
+        //        Quantity = 5
+        //    });
+
+        //    payments.Add(new Payment()
+        //    {
+        //        ProductId = 3,
+        //        Id = 2,
+        //        UserId = 1,
+        //        Quantity = 8
+        //    });
+        //}
+
+        
 
         private List<ProductUsersPaymentViewModel2> ItogViewModel;
         public List<ProductUsersPaymentViewModel2> GetPupViewModel()
-        {
-            var resault = from pay in payments
-                join use in users on pay.UserId equals use.Id
-                join pro in products on pay.ProductId equals pro.Id
+        { 
+            
+            var resault = from pay in _payments
+                join use in _users on pay.UserId equals use.Id
+                join pro in _products on pay.ProductId equals pro.Id
                 select new ProductUsersPaymentViewModel2
                 {
                     Name = pro.Name,
